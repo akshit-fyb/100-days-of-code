f2a423cf85c54de3b42db94ba54ba7e9702aa10f
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 16:52:44 +0530

    Create logical 13.c

---
+++ logical 13.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, k, i, j, temp;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    // simple bubble sort
+    for(i = 0; i < n - 1; i++)
+    {
+        for(j = 0; j < n - i - 1; j++)
+        {
+            if(arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("kth smallest element = %d", arr[k - 1]);
+    return 0;
+}

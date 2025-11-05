baf272dcca3922599eed32753a62db487efb7510
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 15:45:39 +0530

    sum of all elements in matrix.c

--- sum of all elements in matrix.c
+++ sum of all elements in matrix.c
@@ -1,25 +1,17 @@
 #include <stdio.h>
 int main()
 {
-    int r, c, i, j;
+    int r, c, i, j, sum = 0;
     printf("Enter number of rows and columns: ");
     scanf("%d %d", &r, &c);
-    int mat[r][c], rowSum[r];
+    int mat[r][c];
     printf("Enter elements of the matrix:\n");
     for(i = 0; i < r; i++)
         for(j = 0; j < c; j++)
             scanf("%d", &mat[i][j]);
-    
     for(i = 0; i < r; i++)
-    {
-        rowSum[i] = 0;
         for(j = 0; j < c; j++)
-            rowSum[i] += mat[i][j];
-    }
-    
-    printf("Sum of each row:\n");
-    for(i = 0; i < r; i++)
-        printf("Row %d sum = %d\n", i + 1, rowSum[i]);
-    
+            sum += mat[i][j];
+    printf("Sum of all elements = %d", sum);
     return 0;
 }

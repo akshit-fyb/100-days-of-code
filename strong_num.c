efffe83a163a00a670203786bc2edf552532b766
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 13:21:02 +0530

    Add strong number checker in strong_num.c
    Implement a program to check if a number is strong.

---
+++ strong_num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int num, temp, rem, fact, i, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while(temp != 0)
+    {
+        rem = temp % 10;
+        fact = 1;
+        for(i = 1; i <= rem; i++)
+            fact = fact * i;
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+    if(sum == num)
+        printf("Strong number");
+    else
+        printf("Not a strong number");
+    return 0;
+}

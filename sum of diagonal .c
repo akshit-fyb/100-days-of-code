f0797ef9afe6fdabdd901148e9e9cb150e87fdc1
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 15:57:02 +0530

    Create sum of diagonal .c

---
+++ sum of diagonal .c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, sum = 0;
+    printf("Enter size of square matrix: ");
+    scanf("%d", &n);
+    int mat[n][n];
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            scanf("%d", &mat[i][j]);
+
+    for(i = 0; i < n; i++)
+        sum += mat[i][i];
+
+    printf("Sum of main diagonal elements = %d", sum);
+    return 0;
+}
+

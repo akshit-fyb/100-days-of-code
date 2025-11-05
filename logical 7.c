0cdb4ae6acaf6049cd8a6c05c06aa448eb689701
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 16:48:25 +0530

    Create logical 7.c

---
+++ logical 7.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        int prev = -1;
+        for(j = i - 1; j >= 0; j--)
+        {
+            if(arr[j] > arr[i])
+            {
+                prev = arr[j];
+                break;
+            }
+        }
+        printf("%d", prev);
+        if(i != n - 1)
+            printf(", ");
+    }
+
+    return 0;
+}

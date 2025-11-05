6555acb8bf0e5a72fdab003de3297caeeb5be95d
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 16:44:29 +0530

    Create logical 2.c

---
+++ logical 2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, x;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter %d sorted elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    int index = -1;
+    for(i = 0; i < n; i++)
+    {
+        if(arr[i] >= x)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    printf("%d", index);
+    return 0;
+}

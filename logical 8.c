8ef189892b50b8b2800b0492407b2b03eacae5eb
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-11-05 16:49:09 +0530

    Create logical 8.c

---
+++ logical 8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int nums[n], answer[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++)
+        scanf("%d", &nums[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        int prod = 1;
+        for(j = 0; j < n; j++)
+        {
+            if(j != i)
+                prod *= nums[j];
+        }
+        answer[i] = prod;
+    }
+
+    printf("Output array: ");
+    for(i = 0; i < n; i++)
+    {
+        printf("%d", answer[i]);
+        if(i != n - 1)
+            printf(", ");
+    }
+
+    return 0;
+}

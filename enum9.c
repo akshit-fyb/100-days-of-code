a487343572e1e8c695ac98a23200cb411091a488
akshit-fyb <AKSHIT.28830@stu.upes.ac.in>
2025-12-01 22:50:22 +0530

    Create enum9.c

---
+++ enum9.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+enum Levels { LOW = 5, MEDIUM = 10, HIGH = 15 };
+
+int main()
+{
+    printf("LOW = %d\n", LOW);
+    printf("MEDIUM = %d\n", MEDIUM);
+    printf("HIGH = %d\n", HIGH);
+
+    return 0;
+}
